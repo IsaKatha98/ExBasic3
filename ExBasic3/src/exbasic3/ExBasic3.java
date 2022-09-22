edafbac19f5ef43a3dedd3331ae4817de4b08256
ikloerzer <ikloerzer@106-14.info2>
2022-09-22 13:47:18 +0200

    C2

--- ExBasic3/src/exbasic3/ExBasic3.java
+++ ExBasic3/src/exbasic3/ExBasic3.java
@@ -6,21 +6,32 @@ public class ExBasic3 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc= new Scanner (System.in);
-		System.out.println("Introduzca el año actual y pulse Intro");
-		int actyear= sc.nextInt();
-		System.out.println();
-		System.out.println("Introduzca su año de nacimiento y pulse Intro");
-		int birthyear= sc.nextInt();
-		System.out.println();
+		//Le pedimos que introduzca el año actual
+		System.out.print("Introduzca el año actual y pulse Intro: ");
 		
+		//Creamos el escáner
+		Scanner sc= new Scanner (System.in);
+		
+		//Declaramos la variable actyear donde se guarda el año actual
+		int actyear;
+		actyear= sc.nextInt();
+		
+		//Le pedimos que introduzca el año de nacimiento
+		System.out.print("Introduzca su año de nacimiento y pulse Intro: ");
+		
+		//Declaramos la variable birthyear donde se guarda el año de nacimiento 
+		int birthyear;
+		birthyear=sc.nextInt();
 	
+		//Declaramos la variable edad que es la resta del año actual menos el año de nacimiento
 		int edad= actyear-birthyear;
+		
+		//Mostramos el resultado en la consola
 		System.out.println("Su edad es de "+edad+" años.");
+		
+		//Cerramos el escáner
 		sc.close();
 		
-
-
 	}
 
 }
